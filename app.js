25de3593c0caa486a1ad86d3a475d5bf0130244c
Yogesh KU <yogeshyogi634@gmail.com>
2023-06-02 20:20:08 +0530

    added all files

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routers = require("./src/routes/posts.routes");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use("/", routers);
+
+mongoose.connect(process.env.DB_URL+process.env.DB_NAME,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    writeConcern: { w:"majority"}
+})
+.then(()=>{
+    console.log("Connected to DB");
+})
+.catch(()=>{
+    console.log("Cannot be connected to DB");
+}); 
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server connected and running on the port ", process.env.PORT);
+});
+
+module.exports = app;
